3aff45cb1a08719f635abbdd786309f8373ea649
Anawin Vanichyanukroh <Avanichyanukroh@gmail.com>
2018-03-16 17:08:52 -0700

    changed API to giantbomb API to test, still need CORS to work

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const IGDB_SEARCH_URL = 'https://api-endpoint.igdb.com/games/search';
+const GIANTBOMB_SEARCH_URL = 'http://www.giantbomb.com/api/search'; 
 
 function getDataFromApi(searchTerm, callback) {
   
   const settings = {
-    url: IGDB_SEARCH_URL,
-    headers: {
-      'user-key': '15870042b514b393825fc09f6b04056b',
-        'Accept': 'application/json'
-    },
+    url: GIANTBOMB_SEARCH_URL,
     data: {
-      q: `${searchTerm} in:name`,
-      per_page: 5,
-      part: 'snippet',
-      key: '15870042b514b393825fc09f6b04056b',
-      Accept: 'application/json'
+      api_key: '5ad6937d0f0a1d369741b32af556a8e0e0db4ba8',
+      format: 'json',
+      query: `"${searchTerm}"`,
+      resources: 'game'
     },
     dataType: 'json',
     type: 'GET',
@@ -23,31 +18,32 @@ function getDataFromApi(searchTerm, callback) {
   $.ajax(settings);
 }
 
-
-function renderResult(thumbnail_url, video_url) {
+function renderResult(name, icon_url) {
   return `
     <div>
-      <a href="https://www.youtube.com/watch?v=${video_url}"><img src="${thumbnail_url}"/> </a>
+      ${name} <img src="${icon_url}"/>
     </div>
   `;
 }
 
-function displayYoutubeSearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item.snippet.thumbnails.default.url, item.id.videoId));
-  
-  $('.js-search-results').prop('hidden', false);
-  $('.js-search-results').html(results);
+function displayGiantBombSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(results.name, results.image.icon_url));
 }
 
+
 function watchSubmit() {
-  $('.js-search-form').submit(event => {
-    event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('.js-query');
-    const query = queryTarget.val();
-    // clear out the input
-    queryTarget.val("");
-    getDataFromApi(query, displayYoutubeSearchData);
-  });
+  $('.js-search-form').submit(function(event) {
+      event.preventDefault();
+      const userInput = $(this).find('#js-query');
+      const query =  userInput.val();
+      userInput.val("");
+      getDataFromApi(query, displayGiantBombSearchData);
+      });
 }
+
+
+
+
+
 
 $(watchSubmit);
